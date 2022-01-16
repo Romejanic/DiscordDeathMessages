6fc2e4d10bc89c49b121391455e21cf7e054b0fc
Jack Davenport <jack.davenport5011@gmail.com>
2022-01-17 05:05:08 +1100

    Removed unnecessary @SuppressWarning

--- src/com/romejanic/ddm/DiscordDeathMessages.java
+++ src/com/romejanic/ddm/DiscordDeathMessages.java
@@ -25,7 +25,6 @@ public class DiscordDeathMessages extends JavaPlugin {
 	public PlayerUpdateNotifier updateNotifier;
 	private HatStateListener hatState;
 	
-	@SuppressWarnings("unused")
 	private Metrics metrics;
 	
 	@Override
