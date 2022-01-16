61ddbe651e44210be3b56b811080930c1836fadc
Jack Davenport <jack.davenport5011@gmail.com>
2022-01-17 05:33:05 +1100

    Removed old import

--- src/com/romejanic/ddm/update/UpdateChecker.java
+++ src/com/romejanic/ddm/update/UpdateChecker.java
@@ -1,7 +1,6 @@
 package com.romejanic.ddm.update;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
